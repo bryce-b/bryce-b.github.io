ed69532f0969abd7ab5e180267a04f3c92e6e0b4
Bryce <46054945+bryce-b@users.noreply.github.com>
2019-05-13 14:31:54 -0400

    Add background and nav

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,30 @@
 $( document ).ready( function( ) {
-    var $length = $( "ul.preview li" ).length,
-        $nav = $( "ul.nav" );
-	
+    "use strict"
+    
+    var $nav = $( "ul.nav" ),
+        $hamburger = $( ".hamburger" );
+    
     $nav.hide( );
-    $( ".hamburger" ).click( function( ) {
+    $hamburger.hide( );
+    
+    $( window ).scroll( function( ) {
+        var $scroll = $( window ).scrollTop( ),
+            $height = $( window ).height( );
+        
+        if( $scroll > $height / 2 ) {
+            $( ".particles-js" ).hide( );
+            $hamburger.show( );
+        } else {
+            $( ".particles-js" ).show( );
+            $hamburger.hide( );
+        }
+    } );
+    
+    $hamburger.click( function( ) {
         $nav.slideToggle( "fast", function( ) { } );
     } );
 
     $( "ul.nav li a" ).click( function( ) {
         $nav.slideToggle( "fast", function( ) { } );
     } );
-    
-}
+} );
